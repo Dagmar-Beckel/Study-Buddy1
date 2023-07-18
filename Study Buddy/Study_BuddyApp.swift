8233ef3413434a1db05af8d9d5ce94827ca8b11b
Dagmar-Beckel <beckeldagmar@gmail.com>
2023-07-17 20:18:17 -0500

    Initial Commit

---
+++ Study Buddy/Study_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_BuddyApp.swift
+//  Study Buddy
+//
+//  Created by Dagmar Beckel on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Study_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
